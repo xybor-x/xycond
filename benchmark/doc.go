69eef16247adccc1830ff2c0007cc4de5ef1871a
huykingsofm <huykingsofm@gmail.com>
2022-10-10 13:54:40 +0000

    Update version 1.0.0

--- benchmark/doc.go
+++ benchmark/doc.go
@@ -18,34 +18,5 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-package xycond_test
-
-import (
-	"fmt"
-	"testing"
-
-	"github.com/xybor-x/xycond"
-)
-
-func Example() {
-	xycond.AssertFalse(1 == 2)
-
-	var x int
-	xycond.AssertZero(x)
-
-	// Test a condition with *testing.T or *testing.B.
-	var t = &testing.T{}
-	xycond.ExpectEmpty("").Test(t)
-
-	// Perform actions on an expectation.
-	xycond.ExpectEqual(1, 2).
-		True(func() {
-			fmt.Printf("1 == 2")
-		}).
-		False(func() {
-			fmt.Printf("1 != 2")
-		})
-
-	// Output:
-	// 1 != 2
-}
+// Package benchmark is used to benchmark xycond.
+package benchmark
